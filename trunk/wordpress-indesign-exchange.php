c28bf6d7a05176a7627c8b1b72b7310ef2180416
Daniel Baron <daniel@barooney.de>
2015-06-05 12:14:32 +0200

    added the dropbox sdk to the repository via composer. #43

--- trunk/wordpress-indesign-exchange.php
+++ trunk/wordpress-indesign-exchange.php
@@ -30,6 +30,8 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/wordpress-indesign-exchange-activator.php
